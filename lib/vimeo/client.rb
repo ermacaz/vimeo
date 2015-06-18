ddc999b8879511907d8bb64bb90c1e7f83a98a42
MattHamada <mattahamada@gmail.com>
2015-06-17 17:17:42 -0700

    merged v3

--- lib/vimeo/client.rb
+++ lib/vimeo/client.rb
@@ -1,7 +1,10 @@
+require "oauth2"
+
 module Vimeo
   ##
   # This class provides the public APIs' used to interface with the Vimeo API
   class Client
+    include Vimeo::Endpoints::Videos
     include Vimeo::Endpoints::Categories
     include Vimeo::Endpoints::Channels
     include Vimeo::Endpoints::Groups
@@ -10,19 +13,37 @@ module Vimeo
     include Vimeo::Endpoints::Me
     include Vimeo::Upload
 
-    # Vimeo access token (see: https://developer.vimeo.com/apps)
+    # base url for the vimeo api endpoint
+    BASE = "https://api.vimeo.com"
+
+    # Access Token stores the token (see authentication) that *must* be provided
+    # to make requests to the Vimeo API
     attr_accessor :access_token
+
+    class << self
+      ##
+      # If OAuth authentication is needed this method returns an instance of an Oauth2
+      # client (see https://github.com/intridea/oauth2) which as the appropiate
+      # options set.
+      # see authentication for more information.
+      def new_oauth_request client_id, client_secret, identifier, scope = ["public"]
+        params = { scope: scope.join(' '), state: identifier }
+        OAuth2::Client.new(client_id, client_secret, site: BASE, connection_opts: { params: params })
+      end
+    end
 
     ##
     # Create a new Client object. You can either supply a hash of +options+
-    # or a block as per the example:
+    # or a block, see example:
     #
-    # Vimeo::Client.new do |client|
-    #   client.access_token = 'myvimeoaccesstoken'
-    # end
+    #   Vimeo::Client.new do |client|
+    #     client.access_token = 'mytoken'
+    #   end
     #
-    # options = { access_token: 'myvimeoaccesstoken' }
-    # Vimeo.client.new options
+    #   // or
+    #
+    #   Vimeo.client.new { access_token: 'mytoken' }
+    #
     def initialize options = {}
       options.each do |k,v|
         instance_variable_set "@#{k}", v
@@ -30,6 +51,7 @@ module Vimeo
       yield self if block_given?
     end
 
+    # :nodoc:
     def access_token?
       !!(access_token)
     end
--- lib/vimeo/request.rb
+++ lib/vimeo/request.rb
@@ -1,46 +1,64 @@
-require "faraday"
-require "json"
-require 'uri'
+require 'faraday'
 
 module Vimeo
   ##
-  #
+  # This class is the one creating
   class Request
-    BASE = "https://api.vimeo.com"
 
-    def initialize(client, method, path, params = {}, body)
+    ##
+    # this stores the faraday connection instance that will be making the
+    # request to the Vimeo api
+    attr_accessor :performer
+
+    ##
+    #
+    def initialize client, method, path, params, body
       # set our dependencies
       @client, @method, @path, @params, @body = client, method, path, params, body
       # we're required to have the access token now so raise an exception
       # if it hasn't been set
       raise Vimeo::Error::AccessTokenNotSet unless @client.access_token?
       # create a new faraday instance to use as our HTTP client
-      @conn = Faraday.new(url: BASE) do |faraday|
+      @performer = Faraday.new url: Vimeo::Client::BASE do |faraday|
         faraday.request  :multipart
         faraday.request  :url_encoded
         faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
       end
     end
 
+    ##
+    #
     def perform
-      # construct the headers for the requset for given params
-      headers = Vimeo::Headers.new(@client).request_headers
-      # make params an empty string if its empty
-      params = @params.empty? ? "" : @params
-      # faraday uses a different set of method signitures depending on what
-      # type of method is being called
-      args = method_expects_body? ?
-        [@method, @path, params, headers] : [@method, @path, @body, headers]
-      # perform the request with faraday
-      response = @conn.public_send *args
-      # parse the response2
-      Vimeo::Response.new(response).parse_response_or_fail
+      begin
+        perform_request
+      rescue Vimeo::Exception
+        false
+      end
+    end
+
+    def perform!
+      perform_request
     end
 
     protected
     # :nodoc:
     def method_expects_body?
-      [:post, :put, :path].include?(@method)
+      %i(post put path).include?(@method)
+    end
+
+    private
+
+    def perform_request
+      # construct the headers for the requset for given params
+      headers = Vimeo::Headers.new(@client).request_headers
+      # faraday uses a different set of method signitures depending on what
+      # type of method is being called
+      args = method_expects_body? ?
+         [@method, @path, @body, headers] : [@method, @path, @params, headers]
+      # perform the request with faraday
+      response = @performer.public_send *args
+      # parse the response
+      Vimeo::Response.new(response.to_hash).parse_response_or_fail
     end
   end
 end
